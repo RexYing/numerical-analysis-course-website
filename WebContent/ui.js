3cc0e2492d497d427d5a222aa901e5465f706bb8
Rex Ying <zy26@duke.edu>
2014-12-23 22:43:58 +0800

    synopsis page skeleton

--- WebContent/ui.js
+++ WebContent/ui.js
@@ -1,3 +1,19 @@
 $(function() {
-    $( "#tabs" ).tabs();
+    $( "#tabs" ).tabs({
+      beforeLoad: function( event, ui ) {
+        ui.jqXHR.error(function() {
+          ui.panel.html(
+            "Couldn't load this tab. We'll try to fix this as soon as possible. ");
+        });
+      }
+    });
+    
+    $( "#accordion" ).accordion({ header: "h3" });
   });
+
+function autoResizeDiv()
+{
+    document.getElementById('main').style.height = window.innerHeight +'px';
+}
+window.onresize = autoResizeDiv;
+autoResizeDiv();
